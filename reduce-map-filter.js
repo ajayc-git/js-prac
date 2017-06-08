ddc844778551b5d2613fd35f3faf1bb894011179
axc41840 <axc41840@users.noreply.github.com>
2017-06-08 09:24:57 -0500

    Create reduce-map-filter.js

---
+++ reduce-map-filter.js
@@ -0,0 +1,54 @@
+function reducer(accumulator,value,index,array) {
+  //Mean
+  var sum = accumulator + value;
+  if(index == (array.length-1)) {
+    return sum/array.length;
+  }
+     return sum;
+}
+
+var nums = [12,13,54,67,21];
+
+var mean = nums.reduce(reducer,0);
+console.log(mean);
+
+
+console.log(nums.map(function (v) {
+  return v*2;
+}));
+
+console.log(nums.filter(function (v) {
+  return v%2===0;
+}));
+
+function reducer2(coll,value) {
+  //Sum and Multiply
+  
+  if(value%2===0) {
+   coll.push(value*2);
+  }
+     return coll;
+}
+
+var MapFilter = nums.reduce(reducer2,[]);
+console.log(MapFilter);
+
+var items = [
+  'grain',
+  'fruit',
+  'vegetable',
+  'fruit',
+  'vegetable',
+  'vegetable',
+];
+
+function reducer3(arr,val) {
+  if(!arr[val]) {
+    arr[val]=1;
+  } else {
+    arr[val]++;
+  }
+  return arr;
+}
+
+console.log(items.reduce(reducer3,{}));
